2314dd233eab1ebcf1a6bdaca2d30daf6251879e
peng-gu3 <grrrrlee333@gmail.com>
2026-08-11 17:07:57 +0900

    설치형 앱(PWA) 지원 추가

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+
+const CACHE = "gyeongmae-classic-v1";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/app-icon.svg"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).pathname.startsWith("/api/")) return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
+

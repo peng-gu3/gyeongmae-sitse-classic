82ec88be7f7592a2decc41aaa7b07d344274079e
peng-gu3 <grrrrlee333@gmail.com>
2026-08-11 17:07:48 +0900

    설치형 앱(PWA) 지원 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -6,3 +7,10 @@ import "./styles.css";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>,
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(() => {});
+  });
+}
+
